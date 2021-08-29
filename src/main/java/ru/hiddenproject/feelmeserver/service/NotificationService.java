cbda7b574307d9f44cc18d0317db098c80c066a5
CrissNamon <deus.ex.00@mail.ru>
2021-08-29 17:10:47 +0300

    Changed return type to ResponseDto in NotificationService send() method

--- src/main/java/ru/hiddenproject/feelmeserver/service/NotificationService.java
+++ src/main/java/ru/hiddenproject/feelmeserver/service/NotificationService.java
@@ -1,10 +1,11 @@
 package ru.hiddenproject.feelmeserver.service;
 
 import ru.hiddenproject.feelmeserver.dto.NotificationDto;
+import ru.hiddenproject.feelmeserver.dto.ResponseDto;
 import ru.hiddenproject.feelmeserver.exception.NotificationException;
 
-public interface NotificationService {
+public interface NotificationService<R> {
 
-    void send(NotificationDto notificationDto, String receiver) throws NotificationException;
+    ResponseDto<R> send(NotificationDto notificationDto, String receiver) throws NotificationException;
 
 }
--- src/main/java/ru/hiddenproject/feelmeserver/service/impl/NotificationServiceImpl.java
+++ src/main/java/ru/hiddenproject/feelmeserver/service/impl/NotificationServiceImpl.java
@@ -4,19 +4,21 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 import org.springframework.core.io.ClassPathResource;
 import ru.hiddenproject.feelmeserver.dto.NotificationDto;
+import ru.hiddenproject.feelmeserver.dto.ResponseDto;
 import ru.hiddenproject.feelmeserver.exception.NotificationException;
 import ru.hiddenproject.feelmeserver.service.NotificationService;
 
 import java.io.IOException;
 
-public class NotificationServiceImpl implements NotificationService {
+public class NotificationServiceImpl implements NotificationService<String> {
 
     @Override
-    public void send(NotificationDto notificationDto, String token) throws NotificationException {
+    public ResponseDto<String> send(NotificationDto notificationDto, String token) throws NotificationException {
         try {
             Notification notification = Notification
                     .builder()
@@ -30,9 +32,14 @@ public class NotificationServiceImpl implements NotificationService {
                     .setNotification(notification)
                     .putAllData(notificationDto.getData())
                     .build();
-            firebaseMessaging().sendAsync(message);
+
+            return new ResponseDto<>("OK",
+                    firebaseMessaging().send(message)
+            );
         } catch (IOException e) {
             throw new NotificationException("Internal error. Account resource not found");
+        } catch (FirebaseMessagingException e) {
+            throw new NotificationException(e.getMessage());
         }
     }
 
